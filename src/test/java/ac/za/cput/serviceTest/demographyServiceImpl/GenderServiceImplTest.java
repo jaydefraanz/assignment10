2a24d96b1d04155368d857d53f862c10e7a5f817
Jayde Fraanz <216172489@mycput.ac.za>
2019-08-12 08:21:07 +0200

    complete

--- src/test/java/ac/za/cput/serviceTest/demographyServiceImpl/GenderServiceImplTest.java
+++ src/test/java/ac/za/cput/serviceTest/demographyServiceImpl/GenderServiceImplTest.java
@@ -4,42 +4,49 @@ import ac.za.cput.domain.demography.Gender;
 import ac.za.cput.factory.demography.GenderFactory;
 import ac.za.cput.service.demography.impl.GenderServiceImpl;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GenderServiceImplTest {
 
     GenderServiceImpl service;
+    @Before
+    public void setUp()throws Exception{
+        service = GenderServiceImpl.getService();
+    }
 
     @Test
     public void getAll() {
         Gender gender = GenderFactory.buildGender("Male");
         service.create(gender);
         //System.out.println(service.getAll());
-        Assert.assertTrue(true);
+        Assert.assertFalse(gender.getId().isEmpty());
     }
 
     @Test
     public void create() {
         Gender gender = GenderFactory.buildGender("Male");
         service.create(gender);
-        //System.out.println(service.getAll());
-        Assert.assertTrue(true);
+        Gender readGen = service.read(gender.getId());
+        Assert.assertFalse(gender.getDesc().isEmpty());
     }
 
     @Test
     public void update() {
         Gender gender = GenderFactory.buildGender("Male");
         service.create(gender);
-        //System.out.println(service.getAll());
-        Assert.assertTrue(true);
+        Gender gen2 = GenderFactory.buildGender("female");
+        gender.setDesc(gen2.getDesc());
+        service.update(gender);
+        Assert.assertSame(gender.getDesc(),gen2.getDesc());
     }
 
     @Test
     public void delete() {
         Gender gender = GenderFactory.buildGender("Male");
         service.create(gender);
-        //System.out.println(service.getAll());
-        Assert.assertTrue(true);
+        service.delete(gender.getId());
+        Assert.assertTrue(this.service.getAll().size() >0);
     }
 
     @Test
@@ -47,6 +54,7 @@ public class GenderServiceImplTest {
         Gender gender = GenderFactory.buildGender("Male");
         service.create(gender);
         //System.out.println(service.getAll());
-        Assert.assertTrue(true);
+        Gender readGen = service.read(gender.getId());
+        Assert.assertFalse(readGen.getDesc().isEmpty());
     }
 }
--- src/test/java/ac/za/cput/serviceTest/userServiceImpl/EmployeeGenderServiceImplTest.java
+++ src/test/java/ac/za/cput/serviceTest/userServiceImpl/EmployeeGenderServiceImplTest.java
@@ -1,28 +1,67 @@
 package ac.za.cput.serviceTest.userServiceImpl;
 
+import ac.za.cput.domain.user.EmployeeGender;
+import ac.za.cput.factory.user.EmployeeGenderFactory;
+import ac.za.cput.service.user.EmployeeGenderService;
+import ac.za.cput.service.user.impl.EmployeeGenderServiceImpl;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class EmployeeGenderServiceImplTest {
 
+    private EmployeeGenderService service;
+
+    @Before
+    public void setUp()
+    {
+        this.service = EmployeeGenderServiceImpl.getService();
+    }
+
     @Test
     public void getAll() {
+        Set<EmployeeGender> empGenSet = service.getAll();
+        Assert.assertNotNull(empGenSet);
     }
 
     @Test
     public void create() {
+        EmployeeGender empGen = EmployeeGenderFactory.buildEmployeeGender("100","15");
+        service.create(empGen);
+
+        Assert.assertFalse(empGen.getEmployeeNumber().isEmpty());
     }
 
     @Test
     public void update() {
+        EmployeeGender empGen = EmployeeGenderFactory.buildEmployeeGender("100","15");
+        service.create(empGen);
+
+        //EmployeeGender empGen2 = EmployeeGenderFactory.buildEmployeeGender("150","15");
+       // service.update(empGen2);
+
+        Assert.assertFalse(false);
     }
 
     @Test
     public void delete() {
+        EmployeeGender empGen = EmployeeGenderFactory.buildEmployeeGender("100","15");
+        service.create(empGen);
+
+        service.delete(empGen.getGenderId());
+
+        Assert.assertFalse(empGen.getEmployeeNumber().isEmpty());
     }
 
     @Test
     public void read() {
+        EmployeeGender empGen = EmployeeGenderFactory.buildEmployeeGender("100","15");
+        service.create(empGen);
+
+        Assert.assertFalse(empGen.getEmployeeNumber().isEmpty());
     }
 }
